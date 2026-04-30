8aa8a8d0b0fcf2da45d237c65506cf127b1eece2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-04-30 22:57:35 +0900

    Initial Commit

---
+++ GNObjectDetector/GNObjectDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNObjectDetectorApp.swift
+//  GNObjectDetector
+//
+//  Created by joe on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct GNObjectDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
